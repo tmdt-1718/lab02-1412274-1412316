a7aa9951aeb841b0d5c82f5d683b2794790dc73e
1412274 <nhkim141996@gmail.com>
2017-10-25 17:26:56 +0700

    Cập nhật lại giao diện

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  #get 'sessions/new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resources :articles, only: [:index]
-  resources :users, only: [:index]
+  resources :signup, only: [:index]
+  resources :login, only: [:index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
